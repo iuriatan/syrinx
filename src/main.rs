5bdf8bf7db3a876eb73608d748a32076fcdd2888
Iuriatan Felipe Muniz <iuriatan@gmail.com>
2021-11-01 09:39:47 -0300

    Initial commit: syrinx startup

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use config;
+use log;
+use pretty_env_logger;
+use std::path::Path;
+
+mod dgraph_client;
+use dgraph_client::DgraphClient;
+
+/// Default error type
+type CanariaError = Box<dyn std::error::Error + Send + Sync>;
+
+#[tokio::main]
+async fn main() -> Result<(), CanariaError> {
+    pretty_env_logger::init();
+    
+    let settings = load_config();
+
+    let db_client = DgraphClient::new(settings.get_str("dgraph_url")?);
+    if settings.get_bool("set_schema")? || settings.get_bool("drop_all_data")? {
+        if settings.get_bool("drop_all_data")? {
+            db_client.drop_all().await?;
+            log::info!("database dropped")
+        } 
+        let schemafile = settings.get_str("dgraph_schema")?;
+        db_client.set_schema(Path::new(schemafile.as_str())).await?;
+        log::info!("database schema set");
+    }
+
+    // start fs dive
+
+    Ok(())
+}
+
+fn load_config() -> config::Config {
+    let mut settings = config::Config::default();
+    settings
+        .merge(config::File::with_name("Syrinx"))
+        .unwrap()
+        .merge(config::Environment::with_prefix("SYRINX"))
+        .unwrap();
+
+    // TODO: check URL for ending slash and remove it
+    settings
+}
