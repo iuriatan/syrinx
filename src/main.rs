feb131d06a1aa0dfa297cf0126dad1c1768ecadf
Iuriatan Felipe Muniz <iuriatan@gmail.com>
2022-02-21 13:14:43 -0300

    chore(deps): Update config and symphonia

--- src/main.rs
+++ src/main.rs
@@ -15,13 +15,13 @@ async fn main() -> Result<(), CanariaError> {
     pretty_env_logger::init();
     let settings = load_config();
 
-    let db_client = DgraphClient::new(settings.get_str("dgraph_url")?);
+    let db_client = DgraphClient::new(settings.get_string("dgraph_url")?);
     if settings.get_bool("set_schema")? || settings.get_bool("drop_all_data")? {
         if settings.get_bool("drop_all_data")? {
             db_client.drop_all().await?;
             log::info!("database dropped")
         }
-        let schemafile = settings.get_str("dgraph_schema")?;
+        let schemafile = settings.get_string("dgraph_schema")?;
         db_client.set_schema(Path::new(schemafile.as_str())).await?;
         log::info!("database schema set");
     }
@@ -29,11 +29,11 @@ async fn main() -> Result<(), CanariaError> {
     let mut music_ignore_list = settings.get_array("music_ignore_list").unwrap_or_default();
     let music_ignore_list: Vec<String> = music_ignore_list
         .iter_mut()
-        .map(|v| v.clone().into_str().unwrap_or("".into()))
+        .map(|v| v.clone().into_string().unwrap_or("".into()))
         .collect();
     music::library::Library::new(
-        settings.get_str("music_library_path")?,
-        settings.get_str("music_library_name")?,
+        settings.get_string("music_library_path")?,
+        settings.get_string("music_library_name")?,
         &db_client,
         music_ignore_list,
     )
